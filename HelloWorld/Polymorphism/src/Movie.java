5a40d5686eb871b2ea92d6f49566803514efcd7e
WeiJun <wong.weijun923@gmail.com>
2025-09-03 21:49:47 +0800

    Learning Polymorphism

---
+++ HelloWorld/Polymorphism/src/Movie.java
@@ -0,0 +1,66 @@
+public class Movie {
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie(){
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " : " + instanceType);
+    }
+
+    public static Movie getMovie(String title, String type){
+        return switch(type.toUpperCase().charAt(0)){
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
+}
+
+class Adventure extends Movie{
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+}
+
+class Comedy extends Movie{
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Funny Scene",
+                "Funny Music",
+                "Something Funny Happens");
+    }
+}
+
+class ScienceFiction extends Movie{
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3),
+                "Fictional Scene",
+                "Interesting Music",
+                "Something Crazy Happens");
+    }
+}
