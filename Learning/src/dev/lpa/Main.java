b8079f90aa868715317052726f4be8f27a50ce20
WeiJun <wong.weijun923@gmail.com>
2025-09-09 21:51:29 +0800

    Added additional info for Array List + Objects

--- Learning/src/dev/lpa/Main.java
+++ Learning/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 
 package dev.lpa;
 import java.util.Arrays;
+import java.util.ArrayList;
 record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "Dairy", 2);
@@ -10,10 +11,16 @@ record GroceryItem(String name, String type, int count){
 public class Main {
     public static void main(String[] args) {
         int[] myIntArray = new int[10];//not resizable
-        Object[] groceryArray = new Object[3];
+        GroceryItem[] groceryArray = new GroceryItem[3];
         groceryArray[0] = new GroceryItem("milk");
         groceryArray[1] = new GroceryItem("apples", "Produce", 6);
-        groceryArray[2] = "5 oranges";
+        groceryArray[2] = new GroceryItem("cheese");
         System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList<GroceryItem> objectList= new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+//        objectList.add("Yogurt"); wont work has to be GroceryItem
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
     }
 }
